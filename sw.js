e8011708db91578a0b336b302beb6df04d701eb6
fbsercannn-cpu <fbsercannn@gmail.com>
2026-02-04 13:22:15 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'kart-takip-pro-v13';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icon.png',
+  'https://cdn.tailwindcss.com',
+  'https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.css',
+  'https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
